df46bc17383c51b2905dc2c02f42bcfca8b2db19
coder-bean <102741229+coder-bean@users.noreply.github.com>
2024-11-27 19:44:21 +0530

    Update stl10TensorFlow.py

--- stl10TensorFlow.py
+++ stl10TensorFlow.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import tensorflow_datasets as tfds
 import numpy as np
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten
 
 #Load the STL-10 dataset
 
@@ -21,7 +22,7 @@ def dataset_to_numpy(dataset):
         images.append(image)
         labels.append(label)
     return tf.convert_to_tensor(images), tf.convert_to_tensor(labels)
-    
+
 x_train, y_train = dataset_to_numpy(train_dataset)
 x_test, y_test = dataset_to_numpy(test_dataset)
 
@@ -32,28 +33,47 @@ print("y_test:", y_test)
 y_train_encoded = to_categorical(y_train, num_classes=10)
 y_test_encoded = to_categorical(y_test, num_classes=10)
 
-x_train_reshape=np.reshape(x_train,(5000,27648))
-x_test_reshape=np.reshape(x_test,(8000,27648))
+# Normalize: Cast x_train and x_test to float32 before dividing by 255.0
+x_train_norm = tf.cast(x_train, tf.float32) / 255.0
+x_test_norm = tf.cast(x_test, tf.float32) / 255.0
 
-#normalization:
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-x_mean=np.mean(x_train_reshape)
-x_std=np.std(x_train_reshape)
+from tensorflow.keras.layers import BatchNormalization
+datagen = ImageDataGenerator(
+    rotation_range=10,  
+    width_shift_range=0.1,  
+    height_shift_range=0.1,  
+    shear_range=0.1,  
+    zoom_range=0.1,  
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
 
-epsilon=1e-10
 
-x_train_norm=(x_train_reshape-x_mean)/(x_std+epsilon)
-x_test_norm=(x_test_reshape-x_mean)/(x_std+epsilon)
 
 #print(set(x_train_norm[0]))
-model= Sequential([Dense(256, activation='relu', input_shape=[27648,]), Dense(256, activation='relu'), Dense(10, activation='softmax')])
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(96, 96, 3)),
+    BatchNormalization(),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    BatchNormalization(),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+
 
 #compiling model
 model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-#training the model
-model.fit(x_train_norm, y_train_encoded, epochs=10, batch_size=32, validation_data=(x_test_norm, y_test_encoded))
+# Fit the model with augmented data
+model.fit(datagen.flow(x_train_norm, y_train_encoded, batch_size=32), epochs=40, validation_data=(x_test_norm, y_test_encoded))
 
 #evaluating model:
 
